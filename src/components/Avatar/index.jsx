29e8832132725a49c80c87c6569845640e730a85
Naveen2379 <naveen2379@gmail.com>
2021-02-18 16:10:13 +0530

    add title on the image

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -7,9 +7,8 @@ import './index.css';
 const Avatar = () => {
     return (
         <div className='avatar'>
-            <img className='image' src={AvatarImg} alt='avatar'>
-                
-            </img>
+            <img className='image' src={AvatarImg} alt='avatar' />
+            <h2 className='title'><span>Hi! <br/> I'm Naveen Kairamkonda</span></h2>
         </div>
     )
 }
